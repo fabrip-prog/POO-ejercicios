e6ba7753e7b0fc758c0aeaf1f65a0d5e09194b31
Fabrizio Perez <fabriziop800@gmail.com>
2026-04-09 21:50:43 -0300

    primer commit (class creada)

---
+++ auto.js
@@ -0,0 +1,10 @@
+class Auto {
+    constructor(marca, modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    mostrarInfo() {
+        return `Marca: ${this.marca}, Modelo: ${this.modelo}`;
+    }
+}
+
