788f96a0bbbb8ef56d4f3c1da06f4047ed45c3ab
Fabrizio Perez <fabriziop800@gmail.com>
2026-04-09 22:06:13 -0300

    tercer commit (Agregar velocidad)

--- auto.js
+++ auto.js
@@ -2,9 +2,14 @@ class Auto {
     constructor(marca, modelo) {
         this.marca = marca;
         this.modelo = modelo;
+        this.velocidad = 0;
+
     }
     mostrarInfo() {
-        return `Marca: ${this.marca}, Modelo: ${this.modelo}`;
+        return `Marca: ${this.marca}, Modelo: ${this.modelo}, Velocidad: ${this.velocidad} km/h`;
+    }
+    acelerar() {
+        this.velocidad += 100;
     }
 }
 const auto1 = new Auto("Ford", "Fiesta");
