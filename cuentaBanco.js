6839d473bfc12aafe8727c1e2811478a898d18c0
Fabrizio Perez <fabriziop800@gmail.com>
2026-04-16 21:35:11 -0300

    sexto commit banco (historial array priv)

--- cuentaBanco.js
+++ cuentaBanco.js
@@ -1,24 +1,32 @@
 class CuentaBancaria {
     #saldo;
+    #historial;
     constructor(titular, saldoInicial) {
         this.titular = titular;
         this.#saldo = saldoInicial;
+        this.#historial = [];
     }
     depositar(monto) {
         if (monto <= 0) {
             throw new Error("Monto inválido");
         }
         this.#saldo += monto;
+        this.#historial.push({ tipo: "depósito", monto });
     }
     retirar(monto) {
-        if (monto > this.#saldo) {
+        if (monto > this.#saldo && monto <= 1000000) {
             throw new Error("Fondos insuficientes");
         }
         this.#saldo -= monto;
+        this.#historial.push({ tipo: "retiro", monto });
     }
 
     getSaldo() {
         return this.#saldo;
+    }
+
+    getHistorial() {
+        return this.#historial;
     }
 }
 const cuenta1 = new CuentaBancaria("Fabrizio", 1000);
@@ -26,3 +34,4 @@ cuenta1.depositar(500);
 console.log(cuenta1.getSaldo()); 
 cuenta1.retirar(200);
 console.log(cuenta1.getSaldo());
+console.log(cuenta1.getHistorial());
